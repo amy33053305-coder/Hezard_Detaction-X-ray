a97706000830d897bdfc69947454de3dff81ab32
Youngmin Kim <amy33053305@gmail.com>
2026-06-28 09:33:33 +0900

    second commit

--- OneDrive/바탕 화면/AI 프로젝트/항만 위해물품/hello.py
+++ OneDrive/바탕 화면/AI 프로젝트/항만 위해물품/hello.py
@@ -1,6 +1,10 @@
 def master():
     return "im master"
 
+def youngmin():
+    return "im youngmin"
+
 if __name__ == "__main__":
     print("hello world")
     print(master())
+    print(youngmin())
